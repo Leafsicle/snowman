5fbd54b001f8cb5f3b1e8d917d4070690f19bd1f
Benjamin JEHL <benjaminjehl@Benjamins-MacBook-Pro.local>
2018-10-13 14:20:24 -0400

    mapping random word to page

--- src/Game.js
+++ src/Game.js
@@ -4,64 +4,54 @@ import Word from './Word'
 import words from './words.json'
 
 class Game extends Component {
-	state = {
-		letter: [
-			'a',
-			'b',
-			'c',
-			'd',
-			'e',
-			'f',
-			'g',
-			'h',
-			'i',
-			'j',
-			'k',
-			'l',
-			'm',
-			'n',
-			'o',
-			'p',
-			'q',
-			'r',
-			's',
-			't',
-			'u',
-			'v',
-			'w',
-			'x',
-			'y',
-			'z'
-		],
-		playerChoice: [],
-		gameChoice: []
-	}
-	let game = () => {
-		let randomize = Math.floor(Math.random() * words.length)
-		let word = words[randomize]
-		let letters = word.split('')
-		console.log(letters)
-		letters.map(letter => {
-			this.props({
-				gameChoice: letters
-			})
-		})
-	}
-	render() {
-		return (
-			<div class="board">
-				<h1>Do you wanna build a snowman?</h1>
-				<img
-					class="snow"
-					alt="This is a Snow-person"
-					src="./images/step_0.png"
-				/>
-				<Word gameChoice={this.state.gameChoice} />
-				<Keys letter={this.state.letter} />
-				<button className="play-again">Play Again</button>
-			</div>
-		)
-	}
+  state = {
+    letter: [
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n',
+      'o',
+      'p',
+      'q',
+      'r',
+      's',
+      't',
+      'u',
+      'v',
+      'w',
+      'x',
+      'y',
+      'z'
+    ],
+    playerChoice: [],
+    gameChoice: []
+  }
+
+  render() {
+    return (
+      <div class="board">
+        <h1>Do you wanna build a snowman?</h1>
+        <img
+          class="snow"
+          alt="This is a Snow-person"
+          src="./images/step_0.png"
+        />
+        <Word gameChoice={this.state.gameChoice} />
+        <Keys letter={this.state.letter} />
+        <button className="play-again">Play Again</button>
+      </div>
+    )
+  }
 }
 
 export default Game
--- src/Word.js
+++ src/Word.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react'
 import words from './words.json'
 
 class Word extends Component {
-	render() {
-		return (
-			<div>
-				<div class="guess">
-					{this.props.game()}
-					{/* when the word is generated we need to split and map to the game choice array.
+  render() {
+    let game = () => {
+      let randomize = Math.floor(Math.random() * words.length)
+      let word = words[randomize]
+      let letter = word.split('')
+      let mappedLetter = letter.map((letter, index) => {
+        return <p>{letter}</p>
+      })
+      return mappedLetter
+    }
+    console.log(game())
+
+    return (
+      <div>
+        <div class="guess">
+          {game()}
+          {/* when the word is generated we need to split and map to the game choice array.
           once in the array when the player clicks a letter button, the playerchoice array is populated  and compared*/}
-				</div>
-			</div>
-		)
-	}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Word
