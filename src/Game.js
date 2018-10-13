1a3cc9c2c5c4b79dd22a161880dffe68b5e25086
matt <cookem529@gmail.com>
2018-10-13 14:37:10 -0400

    removed class from image

--- src/Game.js
+++ src/Game.js
@@ -4,64 +4,60 @@ import Word from './Word'
 import words from './words.json'
 
 class Game extends Component {
-  state = {
-    letter: [
-      'a',
-      'b',
-      'c',
-      'd',
-      'e',
-      'f',
-      'g',
-      'h',
-      'i',
-      'j',
-      'k',
-      'l',
-      'm',
-      'n',
-      'o',
-      'p',
-      'q',
-      'r',
-      's',
-      't',
-      'u',
-      'v',
-      'w',
-      'x',
-      'y',
-      'z'
-    ],
-    playerChoice: [],
-    gameChoice: []
-  }
+	state = {
+		letter: [
+			'a',
+			'b',
+			'c',
+			'd',
+			'e',
+			'f',
+			'g',
+			'h',
+			'i',
+			'j',
+			'k',
+			'l',
+			'm',
+			'n',
+			'o',
+			'p',
+			'q',
+			'r',
+			's',
+			't',
+			'u',
+			'v',
+			'w',
+			'x',
+			'y',
+			'z'
+		],
+		playerChoice: [],
+		gameChoice: []
+	}
 
-  render() {
-    let game = () => {
-      let randomize = Math.floor(Math.random() * words.length)
-      let word = words[randomize]
-      let letter = word.split('')
-      let mappedLetter = letter.map((letter, index) => {
-        return <p>{letter}</p>
-      })
-      return mappedLetter
-    }
+	render() {
+		let game = () => {
+			let randomize = Math.floor(Math.random() * words.length)
+			let word = words[randomize]
+			let letter = word.split('')
+			let mappedLetter = letter.map((letter, index) => {
+				return <p>{letter}</p>
+			})
+			return mappedLetter
+		}
 
-    return (
-      <div class="board">
-        <h1>Do you wanna build a snowman?</h1>
-        <img
-          class="snow"
-          alt="This is a Snow-person"
-          src="./images/step_0.png"
-        />
-        <Word game={game()} />
-        <Keys letter={this.state.letter} />
-        <button className="play-again">Play Again</button>
-      </div>
-    )
-  }
+		return (
+			<div class="board">
+				<h1>Do you wanna build a snowman?</h1>
+				<img alt="This is a Snow-person" src="./images/step_0.png" />
+				<Word game={game()} />
+				<Keys letter={this.state.letter} />
+				<button className="play-again">Play Again</button>
+			</div>
+		)
+	}
 }
 
 export default Game
