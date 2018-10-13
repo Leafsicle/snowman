0a290e7a5a65c1f79c6c078e21aa557fdbe3cb58
Benjamin JEHL <benjaminjehl@Benjamins-MacBook-Pro.local>
2018-10-13 12:12:48 -0400

    added word component to game

--- src/Game.js
+++ src/Game.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react'
 import Keys from './Keys'
+import Word from './Word'
 
 class Game extends Component {
-	render() {
-		return (
-			<div class="board">
-				<h1>Do you wanna build a snowman?</h1>
-				<img class="snow" src="./images/step_0.png" />
-				<div class="guess">
-					<p>_</p>
-					<p>_</p>
-					<p>_</p>
-					<p>_</p>
-					<p>_</p>
-					<p>_</p>
-					<p>_</p>
-				</div>
-				<Keys />
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div class="board">
+        <h1>Do you wanna build a snowman?</h1>
+        <img class="snow" src="./images/step_0.png" />
+        <Word />
+        <Keys />
+      </div>
+    )
+  }
 }
 
 export default Game
