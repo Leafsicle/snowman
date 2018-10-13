6312bc39392b305b79a130559fa54b7ac2401d3f
matt <cookem529@gmail.com>
2018-10-13 11:56:44 -0400

    hard coded

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Game extends Component {
-  render() {
-    return (
-      <div>
-        <h1></h1>
-        <button></button>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div class="board">
+				<h1>Do you wanna build a snowman?</h1>
+				<img class="snow" src="./images/step_0.png" />
+				<div class="guess">
+					<p>_</p>
+					<p>_</p>
+					<p>_</p>
+					<p>_</p>
+					<p>_</p>
+					<p>_</p>
+					<p>_</p>
+				</div>
+				<div>
+					<button>a</button>
+					<button>b</button>
+					<button>c</button>
+					<button>d</button>
+					<button>e</button>
+					<button>f</button>
+					<button>g</button>
+					<button>h</button>
+					<button>i</button>
+					<button>j</button>
+					<button>k</button>
+					<button>l</button>
+					<button>m</button>
+					<button>n</button>
+					<button>o</button>
+					<button>p</button>
+					<button>q</button>
+					<button>r</button>
+					<button>s</button>
+					<button>t</button>
+					<button>u</button>
+					<button>v</button>
+					<button>w</button>
+					<button>x</button>
+					<button>y</button>
+					<button>z</button>
+				</div>
+			</div>
+		)
+	}
 }
 
-export default Game;
+export default Game
