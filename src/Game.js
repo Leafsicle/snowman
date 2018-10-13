f8c4e8ed269fed0bd74f0aa20b15e6b9c9749fa9
matt <cookem529@gmail.com>
2018-10-13 12:42:12 -0400

    removed component for random letter .better idea

--- src/Game.js
+++ src/Game.js
@@ -3,46 +3,46 @@ import Keys from './Keys'
 import Word from './Word'
 
 class Game extends Component {
-  state = {
-    letter: [
-      'a',
-      'b',
-      'c',
-      'd',
-      'e',
-      'f',
-      'g',
-      'h',
-      'i',
-      'j',
-      'k',
-      'l',
-      'm',
-      'n',
-      'o',
-      'p',
-      'q',
-      'r',
-      's',
-      't',
-      'u',
-      'v',
-      'w',
-      'x',
-      'y',
-      'z'
-    ]
-  }
-  render() {
-    return (
-      <div class="board">
-        <h1>Do you wanna build a snowman?</h1>
-        <img class="snow" src="./images/step_0.png" />
-        <Word />
-        <Keys letter={this.state.letter} />
-      </div>
-    )
-  }
+	state = {
+		letter: [
+			'a',
+			'b',
+			'c',
+			'd',
+			'e',
+			'f',
+			'g',
+			'h',
+			'i',
+			'j',
+			'k',
+			'l',
+			'm',
+			'n',
+			'o',
+			'p',
+			'q',
+			'r',
+			's',
+			't',
+			'u',
+			'v',
+			'w',
+			'x',
+			'y',
+			'z'
+		]
+	}
+	render() {
+		return (
+			<div class="board">
+				<h1>Do you wanna build a snowman?</h1>
+				<img class="snow" src="./images/step_0.png" />
+				<Word />
+				<Keys letter={this.state.letter} />
+			</div>
+		)
+	}
 }
 
 export default Game
