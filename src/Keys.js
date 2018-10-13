f4052c1e7ed56fc2eca9ed85db91d6cf9a0f806f
Benjamin JEHL <benjaminjehl@Benjamins-MacBook-Pro.local>
2018-10-13 12:30:10 -0400

    added letter state to game, passed props to keys

--- src/Keys.js
+++ src/Keys.js
@@ -1,41 +1,10 @@
 import React, { Component } from 'react'
 
 class Keys extends Component {
-  state = {
-    letter: [
-      'a',
-      'b',
-      'c',
-      'd',
-      'e',
-      'f',
-      'g',
-      'h',
-      'i',
-      'j',
-      'k',
-      'l',
-      'm',
-      'n',
-      'o',
-      'p',
-      'q',
-      'r',
-      's',
-      't',
-      'u',
-      'v',
-      'w',
-      'x',
-      'y',
-      'z'
-    ]
-  }
-
   render() {
     return (
       <div class="buttons">
-        {this.state.letter.map(letter => (
+        {this.props.letter.map(letter => (
           <button>{letter}</button>
         ))}
       </div>
