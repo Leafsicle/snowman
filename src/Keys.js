996490e746cc3dbc6df8aba5384467c19e78ff41
Benjamin JEHL <benjaminjehl@Benjamins-MacBook-Pro.local>
2018-10-13 12:26:30 -0400

    mapping letters to board

--- src/Keys.js
+++ src/Keys.js
@@ -1,38 +1,46 @@
 import React, { Component } from 'react'
 
 class Keys extends Component {
-	render() {
-		return (
-			<div class="buttons">
-				<button>a</button>
-				<button>b</button>
-				<button>c</button>
-				<button>d</button>
-				<button>e</button>
-				<button>f</button>
-				<button>g</button>
-				<button>h</button>
-				<button>i</button>
-				<button>j</button>
-				<button>k</button>
-				<button>l</button>
-				<button>m</button>
-				<button>n</button>
-				<button>o</button>
-				<button>p</button>
-				<button>q</button>
-				<button>r</button>
-				<button>s</button>
-				<button>t</button>
-				<button>u</button>
-				<button>v</button>
-				<button>w</button>
-				<button>x</button>
-				<button>y</button>
-				<button>z</button>
-			</div>
-		)
-	}
+  state = {
+    letter: [
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n',
+      'o',
+      'p',
+      'q',
+      'r',
+      's',
+      't',
+      'u',
+      'v',
+      'w',
+      'x',
+      'y',
+      'z'
+    ]
+  }
+
+  render() {
+    return (
+      <div class="buttons">
+        {this.state.letter.map(letter => (
+          <button>{letter}</button>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default Keys
