b8fe87fa32913eff539ea1701033d5c9c50187dc
Benjamin JEHL <benjaminjehl@Benjamins-MacBook-Pro.local>
2018-10-13 12:06:43 -0400

    word comonent created

---
+++ src/Word.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Word extends Component {
+  render() {
+    return (
+      <div>
+        <div class="guess">
+          <p>_</p>
+          <p>_</p>
+          <p>_</p>
+          <p>_</p>
+          <p>_</p>
+          <p>_</p>
+          <p>_</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Word
