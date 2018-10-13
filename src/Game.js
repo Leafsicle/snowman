1784d184629db53f616f8090b998de887b28edfd
matt <cookem529@gmail.com>
2018-10-13 11:39:44 -0400

    added images

---
+++ src/Game.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  render() {
+    return (
+      <div>
+        <h1></h1>
+        <button></button>
+      </div>
+    );
+  }
+}
+
+export default Game;
