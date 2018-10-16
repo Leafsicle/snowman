cc5380655f579a2754df8943991bd16e73ae831e
matt <cookem529@gmail.com>
2018-10-16 08:55:25 -0400

    updating from class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react'
 import './App.css'
-import Game from './Game'
+import Words from './Words.json'
+import WordDisplay from './WordDisplay'
+import Snowman from './Snowman'
+import Keys from './Keys'
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			correctGuessCount: 0,
+			lettersChosen: [],
+			randomWord: Words[Math.floor(Math.random() * Words.length)]
+		}
+	}
+	correctGuessCount = () => {
+		return this.state.randomWord.split('').filter((letter, index) => {
+			return this.state.lettersChosen.includes(letter)
+		}).length
+	}
+
+	letterChosen = letter => {
+		// Attempt to increment current guess count based on the letter
+		// let currentCorrectGuessCount = this.state.correctGuessCount
+		// if (this.state.randomWord.includes(letter)) {
+		// 	currentCorrectGuessCount++
+		// }
+		this.state.lettersChosen.push(letter)
+		this.setState({
+			lettersChosen: this.state.lettersChosen
+		})
+	}
+
 	render() {
 		return (
 			<div className="App">
-				<Game />
+				<Snowman correctGuessCount={this.correctGuessCount()} />
+				<WordDisplay
+					randomWord={this.state.randomWord}
+					lettersChosen={this.state.lettersChosen}
+				/>
+				<Alphabet
+					letterChosen={this.letterChosen}
+					lettersChosen={this.state.lettersChosen}
+				/>
 			</div>
 		)
 	}
